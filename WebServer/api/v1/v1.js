cedd7078f513fd409b8136d4fef5d906986eba09
rgupta <communication.rishabh@gmail.com>
2017-03-05 13:31:37 +0530

    Add randm function in api

--- WebServer/api/v1/v1.js
+++ WebServer/api/v1/v1.js
@@ -3,6 +3,14 @@ const vehiclesJSON = require('./vehiclesInfo/vehicles.json');
 
 v1Routes.route('/vehicle-info')
         .get((req, res, next) => {
+            const vehiclesInfo = [];
+            console.log(vehiclesJSON);
+            console.log(typeof vehiclesJSON);
+            vehiclesJSON.map((vehicle, index) => {
+                    vehicle.latitude = Math.floor(Math.random() * 90) + 1
+                    vehicle.longitude = Math.floor(Math.random() * 100) + 1;
+            })
+
             res.json(vehiclesJSON);
         });
 
